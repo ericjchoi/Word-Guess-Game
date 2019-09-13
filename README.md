981f157e10f3e68f93e377e30544e4a80a4715d4
Eric Choi <39499807+ericjchoi@users.noreply.github.com>
2019-09-13 05:06:47 -0700

    HW3 Word Guessing Game - Guessing the Fruit Name completed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Word-Guess-Game
+
+HW3 Name of Fruit Guessing Game using HTML, CSS and javascript.
+
+Computer randomly decides a name of fruit from total 39 fruits in the array.
+
+Current word space is initially provided with underscores according to its length and updated with correct guesses upon matching.
+
+Number of guesses remaining reduces as the game progresses.
+
+Number of winning is increased upon guessing the full correct name.
+
+Originally planned to display the pictures of each fruit when user wins the game, but could not meet the due date.
---
+++ assets/javascript/game.js
@@ -0,0 +1,134 @@
+/* HW3 Word Guess Game - Name of Fruit Guessing Game */
+
+/* set up the fruit array */
+var fruit = ["apple", "apricot", "banana", "blackberry", "blueberry",
+    "coconut", "cherry", "clementine", "dragonfruit", "durian", "elderberry",
+    "fig", "gooseberry", "grape", "grapefruit", "huckleberry", "jackfruit", "jujube",
+    "kiwifruit", "lemon", "lime", "lychee", "mango", "melon", "cantaloupe", "honeydew",
+    "watermelon", "nectarine", "orange", "passionfruit", "peach", "pear", "plum", "prune",
+    "pineapple", "pomegranate", "raspberry", "strawberry", "tangerine"];
+
+/* initial set up vaiables, arrays and counters */
+var count = 0;
+var wins = 0;
+var correct = false;
+var guessRemaining = 30;
+var decidedFruit = "";
+var underlineArray = [];
+var keyPressed = "";
+var compare = [];
+var outArray = [];
+var guess = [];
+var guessOut = [];
+var attempt = 0;
+
+/* Function for Computer decides name of fruit */
+function decideFruit() {
+    decidedFruit = fruit[Math.floor(Math.random() * 39)];
+    var x = decidedFruit;
+
+    /* store decided word into comparing array */
+    var y = x;
+    compare = [];
+    for (var i = 0; i < y.length; i++) {
+        compare[i] = y.charAt(i);
+        outArray[i] = "*";
+    }
+}
+
+/* Function for Current word - display initial underlines */
+function currentWordUnderlines() {
+    for (var j = 0; j < decidedFruit.length; j++) {
+        underlineArray[j] = "_";
+    }
+    document.getElementById("currentWordID").innerHTML = underlineArray.join(" ");
+}
+
+function initialize() {
+    correct = false;
+    guessRemaining = 30;
+    decidedFruit = "";
+    underlineArray = [];
+    keyPressed = "";
+    compare = [];
+    outArray = [];
+    guess = [];
+    guessOut = [];
+    attempt = 0;
+    decideFruit();
+    currentWordUnderlines();
+    guessUpdate();
+    document.getElementById("winsID").innerHTML = wins;
+    document.getElementById("guessRemainingID").innerHTML = guessRemaining;
+    document.getElementById("result0").innerHTML = "";
+    document.getElementById("result1").innerHTML = "";
+}
+
+/* initial display of counters before the game starts */
+document.getElementById("winsID").innerHTML = wins;
+document.getElementById("guessRemainingID").innerHTML = guessRemaining;
+document.getElementById("result1").innerHTML = "Start the game with your first guessing letter.";
+
+/* Function for checking user-decided letters and computer-decided letters */
+function check() {
+    for (var k = 0; k < compare.length; k++) {
+        if (keyPressed === compare[k]) {
+            if (outArray[k] === "*" || outArray[k] === "_")
+                outArray[k] = keyPressed;
+        }
+        else {
+            if (outArray[k] === "*" || outArray[k] === "_")
+                outArray[k] = "_";
+        }
+    }
+    document.getElementById("currentWordID").innerHTML = outArray.join(" ");
+}
+
+/* Function for Updating Letters Already Guessed */
+function guessUpdate() {
+    var duplicate = false;
+    guess[attempt] = keyPressed;
+    for (var l = 0; l < guess.length; l++) {
+        if (attempt !== l) {
+            if (guessOut[l] === keyPressed)
+                duplicate = true;
+        }
+    }
+    if (duplicate === false) {
+        guessOut[attempt++] = keyPressed;
+    }
+    document.getElementById("letterGuessedID").innerHTML = guessOut.join(" ");
+}
+
+/* Game starts here with keyboard press */
+document.onkeypress = function (event) {
+    document.getElementById("result0").innerHTML = "";
+    document.getElementById("result1").innerHTML = "";
+
+    if (count === 0) {
+        initialize();
+    }
+    count++;
+
+    keyPressed = event.key.toLowerCase();
+    guessUpdate();
+    check();
+    document.getElementById("guessRemainingID").innerHTML = --guessRemaining;
+
+    if (outArray.toString() === compare.toString()) {
+        document.getElementById("winsID").innerHTML = ++wins;
+        var tempOut = decidedFruit;
+        initialize();
+        document.getElementById("result0").innerHTML = "Congratulations, your answer " + tempOut + " was Correct !!";
+        document.getElementById("result1").innerHTML = "Restart the game with your first guessing letter.";
+    }
+
+    if (guessRemaining === 0) {
+        document.getElementById("guessRemainingID").innerHTML = 0;
+        document.getElementById("result0").innerHTML = "SORRY, YOU LOST !! The Answer was " + decidedFruit + ".";
+        document.getElementById("result1").innerHTML = "Press Any Key to Restart";
+    }
+    if (guessRemaining === -1) {
+        initialize();
+    }
+}
